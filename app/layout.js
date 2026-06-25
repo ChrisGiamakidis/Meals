733bd05493aaf4abf282bf73f5a5584d9738d0e7
ChrisGiamakidis <giamaros4@gmail.com>
2026-06-25 20:05:06 +0300

    Revert "Install Vercel Web Analytics"

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/next";
 import MainHeader from "@/components/main-header/main-header";
 import { DeletedMealsProvider } from "@/store/deleted-meals-context";
 import { SpeedInsights } from "@vercel/speed-insights/next";
